928c5a1569024fe4993c9a6d3abaface402c17ea
CloudwareESI <cloudwareesi23@gmail.com>
2023-10-03 06:29:30 -0300

    testeo de seguimiento

---
+++ seguimientoDePaquete.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+include "db/funciones_utiles.php";
+require "almacen/super_controlador_almacen.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="icon" type="image/jpg" href="Imagenes/Logo_quickcarry-sin-fondo.png">
+    <link rel="stylesheet" href="estiloSeguimiento.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <title>QuickCarry</title>
+</head>
+
+
+<body>
+    <form action="" method="get">
+
+        <div class="barras">
+            <i  id="barra1" class="fa-solid fa-house-laptop"></i>
+            <div class="barra1">
+                <div class="transicion1"></div>
+            </div>
+            <i id="barra2" class="fa-solid fa-truck-arrow-right"></i>
+            <div class="barra2">
+                <div class="transicion2"></div>
+            </div>
+            <i id="barra3" class="fa-solid fa-boxes-packing"></i>
+
+            <div class="barra3">
+                <div class="transicion3"></div>
+            </div>
+            <i id="barra4" class="fa-solid fa-truck-arrow-right"></i>
+
+            <div class="barra4">
+                <div class="transicion"></div>
+            </div>
+            <i id="barra4"class="fa-solid fa-house-user"></i>
+        </div>
+
+
+
+    </form>
+</body>
+
+</html>
